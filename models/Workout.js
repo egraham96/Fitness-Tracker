02f04456827d18e6b8b71aa93dfb5d0305e5def3
Emma <eeg4@uw.edu>
2021-12-07 01:20:46 -0800

    Completed Workout Schema

---
+++ models/Workout.js
@@ -0,0 +1,44 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const WorkoutSchema = new Schema({
+day: {
+        type: Date,
+        default: Date.now
+    },
+
+exercises: [{
+  name: {
+    type: String,
+    required: "Name of exercise is required"
+  },
+
+  type: {
+    type: String,
+    required: "Type of Exercise is Required",
+  },
+
+ weight: {
+    type: Number
+  },
+
+ set: {
+    type: Number,
+  },
+
+ reps: {
+    type: Number,
+  },
+
+  duration: {
+    type: Number
+},
+distance: {
+    type: Number
+}
+}]    
+});
+
+const Workout = mongoose.model("Workout", workoutSchema);
+module.exports = Workout;
