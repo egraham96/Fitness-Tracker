5931046b3e30d525dc3ebf6c5fb16fc34c71fe48
Emma <eeg4@uw.edu>
2021-12-07 03:04:40 -0800

    Update to ApiRoutes

--- routes/Apiroutes.js
+++ routes/Apiroutes.js
@@ -12,7 +12,27 @@ router.get('/api/workouts', (req, res) => {
       });
   });
 
-/*GET Route for Getting the Range of Workouts router.get('/api/workouts/range', (req, res) => {})*/
+//GET Route for Getting the Range of Workouts 
+router.get("/api/workouts/range", ({params}, res) => { 
+    var d = new Date();
+    d.setDate(d.getDate()-10);
+    db.Workout.aggregate([
+        { 
+            $match: { 'day' : {$gte: d}},
+        },    
+        {
+            $addFields:{
+                totalDuration: { $sum: "$exercises.duration" }
+            }
+        }
+    ])
+    .then(dbWorkout => {
+        res.json(dbWorkout);
+    })
+    .catch(err => {
+    res.status(400).json(err);
+    });
+});
 
 //Post Route for Creating a Workout
 router.post('/api/workouts', ({ body }, res) => {
